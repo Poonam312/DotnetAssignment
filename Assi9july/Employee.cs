8567c4d6c9634d18f0faec80296fa2a28e332a4c
poonamrajkumarpise <punampise5@gmail.com>
2026-07-11 13:04:33 +0530

    Added new code

---
+++ Assi9july/Employee.cs
@@ -0,0 +1,180 @@
+using System.Collections.Generic;
+
+// ************* Abstract Class *************
+abstract class Employee
+{
+    public int EmployeeId { get; set; }
+    public string Name { get; set; }
+    public string Department { get; set; }
+    public int LeaveBalance { get; set; }
+
+    public void DisplayDetails()
+    {
+        Console.WriteLine("--------------------------------");
+        Console.WriteLine("Employee Id   : " + EmployeeId);
+        Console.WriteLine("Name          : " + Name);
+        Console.WriteLine("Department    : " + Department);
+        Console.WriteLine("Leave Balance : " + LeaveBalance);
+    }
+
+    public abstract void SetLeaveBalance();
+}
+
+// ************* Permanent Employee *************
+class PermanentEmployee : Employee
+{
+    public override void SetLeaveBalance()
+    {
+        LeaveBalance = 24;
+    }
+}
+
+// ************* Contract Employee *************
+class ContractEmployee : Employee
+{
+    public override void SetLeaveBalance()
+    {
+        LeaveBalance = 12;
+    }
+}
+
+// ************* Leave Request *************
+class LeaveRequest
+{
+    public int LeaveId { get; set; }
+    public int EmployeeId { get; set; }
+    public int NumberOfDays { get; set; }
+    public string Reason { get; set; }
+
+    public void DisplayLeave()
+    {
+        Console.WriteLine("--------------------------------");
+        Console.WriteLine("Leave Id      : " + LeaveId);
+        Console.WriteLine("Employee Id   : " + EmployeeId);
+        Console.WriteLine("Days          : " + NumberOfDays);
+        Console.WriteLine("Reason        : " + Reason);
+    }
+}
+
+// ************* Main Program *************
+class Program
+{
+    static void Main()
+    {
+        // ************* Task 1 *************
+        List<Employee> employees = new List<Employee>();
+
+        PermanentEmployee e1 = new PermanentEmployee()
+        {
+            EmployeeId = 101,
+            Name = "Poonam",
+            Department = "IT"
+        };
+        e1.SetLeaveBalance();
+
+        PermanentEmployee e2 = new PermanentEmployee()
+        {
+            EmployeeId = 102,
+            Name = "Rahul",
+            Department = "HR"
+        };
+        e2.SetLeaveBalance();
+
+        ContractEmployee e3 = new ContractEmployee()
+        {
+            EmployeeId = 103,
+            Name = "Priya",
+            Department = "Finance"
+        };
+        e3.SetLeaveBalance();
+
+        ContractEmployee e4 = new ContractEmployee()
+        {
+            EmployeeId = 104,
+            Name = "Amit",
+            Department = "Sales"
+        };
+        e4.SetLeaveBalance();
+
+        employees.Add(e1);
+        employees.Add(e2);
+        employees.Add(e3);
+        employees.Add(e4);
+
+        // ************* Task 2 *************
+        Console.WriteLine("===== ALL EMPLOYEES =====");
+
+        foreach (Employee emp in employees)
+        {
+            emp.DisplayDetails();
+        }
+
+        // ************* Task 3 *************
+        List<LeaveRequest> leaveRequests = new List<LeaveRequest>();
+
+        LeaveRequest l1 = new LeaveRequest()
+        {
+            LeaveId = 1,
+            EmployeeId = 101,
+            NumberOfDays = 2,
+            Reason = "Medical"
+        };
+
+        LeaveRequest l2 = new LeaveRequest()
+        {
+            LeaveId = 2,
+            EmployeeId = 103,
+            NumberOfDays = 5,
+            Reason = "Family Function"
+        };
+
+        LeaveRequest l3 = new LeaveRequest()
+        {
+            LeaveId = 3,
+            EmployeeId = 104,
+            NumberOfDays = 1,
+            Reason = "Personal Work"
+        };
+
+        leaveRequests.Add(l1);
+        leaveRequests.Add(l2);
+        leaveRequests.Add(l3);
+
+        // ************* Task 4 *************
+        Console.WriteLine("\n===== ALL LEAVE REQUESTS =====");
+
+        foreach (LeaveRequest leave in leaveRequests)
+        {
+            leave.DisplayLeave();
+        }
+
+        // ************* Task 5 *************
+        Console.WriteLine("\n===== PERMANENT EMPLOYEES =====");
+
+        foreach (Employee emp in employees)
+        {
+            if (emp is PermanentEmployee)
+            {
+                emp.DisplayDetails();
+            }
+        }
+
+        // ************* Task 6 *************
+        Console.WriteLine("\n===== EMPLOYEE ID = 103 =====");
+
+        foreach (Employee emp in employees)
+        {
+            if (emp.EmployeeId == 103)
+            {
+                emp.DisplayDetails();
+                break;
+            }
+        }
+
+        // ************* Task 7 *************
+        Console.WriteLine("\nTotal Employees : " + employees.Count);
+
+        // ************* Task 8 *************
+        Console.WriteLine("Total Leave Requests : " + leaveRequests.Count);
+    }
+}
